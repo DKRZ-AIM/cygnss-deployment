ab6924d53375d9cf9549f75caa3b14a889dd2bee
harsh grover <grover@dkrz.de>
2022-08-30 21:59:26 +0200

    UI with mongo DB connection

--- Prefect-MLFlow Pipeline/dashboard.py
+++ Prefect-MLFlow Pipeline/dashboard.py
@@ -5,87 +5,104 @@ import numpy as np
 import requests
 from sklearn.ensemble import RandomForestClassifier
 import json
+import datetime
+import streamlit as st
+from pymongo import MongoClient, errors
+from PIL import Image 
+import requests
+from io import BytesIO
+
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+
 #app heading
 st.write("""
-# Wine Quality Prediction App
-This app predicts the ***Wine Quality*** type!
-""")
+# Wind Speed""")
 #creating sidebar for user input features
 st.sidebar.header('User Input Parameters')
-  
+
+
+
 def user_input_features():
-        fixed_acidity = st.sidebar.slider('fixed acidity', 4.6, 15.9, 8.31)
-        volatile_acidity = st.sidebar.slider('volatile acidity', 0.12,1.58 , 0.52)
-        citric_acid = st.sidebar.slider('citric acid', 0.0,1.0 , 0.5)
-        chlorides = st.sidebar.slider('chlorides', 0.01,0.6 , 0.08)
-        residual_suagar = st.sidebar.slider('residual_suagar', 0.01,0.6 , 0.08)
-        total_sulfur_dioxide=st.sidebar.slider('total sulfur dioxide', 6.0,289.0 , 46.0)
-        free_sulfur_dioxide=st.sidebar.slider('free sulfur dioxide', 6.0,289.0 , 46.0)
-        density=st.sidebar.slider('density', 8.4,14.9, 10.4)
-        pH=st.sidebar.slider('pH', 8.4,14.9, 10.4)
-        alcohol=st.sidebar.slider('alcohol', 8.4,14.9, 10.4)
-        sulphates=st.sidebar.slider('sulphates', 0.33,2.0,0.65 )
-        data = {'fixed_acidity': fixed_acidity,
-                'volatile_acidity': volatile_acidity,
-                'citric_acid': citric_acid,
-                'chlorides': chlorides,
-                'residual_suagar': residual_suagar,
-              'total_sulfur_dioxide':total_sulfur_dioxide,
-              'free_sulfur_dioxide':free_sulfur_dioxide,
-              'density':density,
-              'pH': pH,    
-              'alcohol':alcohol,
-                'sulphates':sulphates}
-        features = pd.DataFrame(data, index=[0])
-        return features
+        option = st.sidebar.selectbox('What would you like to see?', ('RMSE', 'Visualization'))
+        date_ = st.sidebar.date_input("For which date you want to see the results", datetime.date.today() - datetime.timedelta(days=5), min_value = datetime.date(2021,1,1), max_value = datetime.date.today() - datetime.timedelta(days=5))
+        st.write('', option)
+        return date_
+
+@st.experimental_singleton
+def init_connection(): 
+        client = MongoClient(host='localhost:27017' , serverselectiontimeoutms=3000)
+        return client
 
 
 
-def get_model_predictions(data):
-    """
-    Get model predictions  
-    ENDPOINT = Calls an endpoint to get the predictions
-    REGISTRY = Loads model from registry and predicts
-    MOCKED = Randomly generated prediction
-    """
-    option="ENDPOINT"
+def write_data():
+        data_1 = {
+        "rmse": 3.1, 
+        "event_date":  datetime.datetime(2022, 8, 10),
+        "image_url": "https://www.dkrz.de/en/about-en/aufgaben/dkrz-and-climate-research/@@images/image/large"
+        }
 
-    if option == "ENDPOINT":
-        # Currently not supported for multi-input models
-        DEPLOYED_ENDPOINT = "http://127.0.0.1:5000/invocations"
-        headers = {"Content-Type":"application/json"}
+        data_2 = {
+        "rmse": 2.1,         
+        "event_date":  datetime.datetime(2022, 8, 9),
+        "image_url": "https://www.dkrz.de/en/about-en/aufgaben/dkrz-and-climate-research/@@images/image/large"
+        }
 
-        to_send_data_json = json.dumps({"data":data})                
-        prediction = requests.post(url=DEPLOYED_ENDPOINT, 
-                                   data = to_send_data_json, headers=headers)
-        
-        prediction = prediction.content.decode("utf-8")
-        print(prediction)
-    
-    if option == "REGISTRY":
-        # Currently not supported for multi-input models
-        model_name = "ElasticnetWineModel"
-        model_version = "25"
-        model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/{model_version}")
-        
-        prediction = model.predict(data)
-        
-    return prediction
+        data_3 = {
+        "rmse": 3.2,         
+        "event_date":  datetime.datetime(2022, 8, 8),
+        "image_url": "https://www.dkrz.de/en/about-en/aufgaben/dkrz-and-climate-research/@@images/image/large"
+        }
 
+
+        cygnss_collection = client["cygnss"].cygnss_collection
+
+        cygnss_collection = cygnss_collection.insert_many([data_1, data_2, data_3])
+
+        print(f"Multiple tutorials: {cygnss_collection.inserted_ids}")
+
+
+
+# Pull data from the collection.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def get_data(date_):        
+        cygnss = client.cygnss        
+        from_date = datetime.datetime.combine(date_, datetime.time())
+        criteria = {"event_date": {"$eq": from_date}}
+        items = cygnss.cygnss_collection.find(criteria)        
+        items = list(items)  # make hashable for st.experimental_memo
+        return items
+
+
+# Creating UI
 st.subheader('User Input parameters')
-df = user_input_features()
-st.write(df)
-#reading csv file
-data=pd.read_csv("winequality-red.csv")
+date_ = user_input_features()
+st.write('Date:', date_)
 
-#random forest model
-st.subheader('Wine quality labels and their corresponding index number')
-st.write(pd.DataFrame({
-   'wine quality': [3, 4, 5, 6, 7, 8 ]}))
+# Initializing connection
+client = init_connection()
 
 
-prediction = get_model_predictions(df.values.tolist())
+# drop database if exists, just to not clutter it with multiple values of data
+client.drop_database('cygnss')
 
-st.subheader('Prediction')
-st.write(prediction)
+# write data
+write_data()
 
+# Fetching data
+items = get_data(date_)
+
+
+st.subheader('Results')
+# Print results.
+for item in items:
+        st.write(f"RMSE is: {item['rmse']} ")
+        response = requests.get(item['image_url'])
+        image = Image.open(BytesIO(response.content))
+
+        
+        #displaying the image on streamlit app
+        st.image(image, caption='Visualization')
